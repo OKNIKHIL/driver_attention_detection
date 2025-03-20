c8591a9f7b8e3aa3bd3ae248904a74493b0067ff
NIKHIL BIRADAR <105047199+OKNIKHIL@users.noreply.github.com>
2025-03-20 18:38:00 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st 
+from functions import *
+st.title('Driver detection with image')
+
+
+image = st.file_uploader(label='upload image on which you want to detect',type = ['png','jpg'])
+
+
+if image is not None:
+    st.image(image= image,caption = 'uploaded image')
+    x = predict_with_image(image)
+    st.write(x)
+else:
+    st.write('upload the image first...')
